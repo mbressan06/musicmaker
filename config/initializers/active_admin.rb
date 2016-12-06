3256a506fd256c827c5ab2fb14f41f8d6a6d5eeb
Marcelo Bressan <mbressan06@hotmail.com>
2016-12-06 21:06:46 -0200

    Added locales and logo

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,8 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Active Admin Depot"
+  config.site_title = "Music Maker CRM"
+  config.site_title_image = "logo.png"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -54,7 +55,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the application controller.
-  config.authentication_method = false
+  config.authentication_method = :authenticate_admin_user!
 
   # == User Authorization
   #
@@ -86,7 +87,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # (within the application controller) to return the currently logged in user.
-  config.current_user_method = false
+  config.current_user_method = :current_admin_user
 
   # == Logging Out
   #
