a57ffd83ff50b2621d074c813ab147a6500a525a
Marcelo Bressan <mbressan06@hotmail.com>
2016-12-07 14:32:50 -0200

    Adjustments in pt-BR locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   include Authentication
+
   protect_from_forgery
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
